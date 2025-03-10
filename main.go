cfbedcb173fadcc9f79d176b6eccd0b04ba90afb
Barron Hsu <barron.hsu.15@gmail.com>
2025-03-10 20:54:28 +0800

    variable and type

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	var name string = "Barron"
+	age := 28
+	const pi = 3.1415
+
+	fmt.Println("Name: ", name)
+	fmt.Println("Age: ", age)
+	fmt.Println("PI: ", pi)
 }
